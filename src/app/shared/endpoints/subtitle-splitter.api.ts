f13af0f512981af94c6d0c68a4dd075dbfbdefa9
Alexandre Moraes Matos <alexandremt03@gmail.com>
2019-06-18 23:51:31 -0300

    Modificações na estrutura do projeto

--- src/app/shared/endpoints/subtitle-splitter.api.ts
+++
@@ -1,9 +0,0 @@
-import {environment} from '../../../environments/environment';
-
-export const API = environment.API;
-
-export class SubtitleSplitterApi {
-  public static getSubtitleParts = (url?: string) => {
-    return `${API}/subtitle-split/get-parts/${url}`;
-  }
-}
