f24ed158071b4df4e792b4a7e57d5e81e5ee5b11
Alexandre Moraes Matos <alexandremt03@gmail.com>
2020-01-31 11:00:11 -0300

    remove url cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  API: 'https://cors-anywhere.herokuapp.com/https://api-4subbers.herokuapp.com/api',
+  API: 'https://api-4subbers.herokuapp.com/api',
   production: true
 };
